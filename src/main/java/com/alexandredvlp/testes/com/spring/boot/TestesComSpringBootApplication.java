1e90aee602e6da55fe23a3d1db2e1b73a6a239d1
alecxandy <alecxandy@gmail.com>
2023-03-29 20:41:57 -0300

    Projeto iniciado

---
+++ src/main/java/com/alexandredvlp/testes/com/spring/boot/TestesComSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.alexandredvlp.testes.com.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class    TestesComSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestesComSpringBootApplication.class, args);
+	}
+
+}
